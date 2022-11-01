14b28c9a836dbcc01d1c3476a47a984076f62fb4
Melyr <57968@etu.he2b.be>
2022-11-01 22:21:57 +0100

    Deuxième partie

--- LeJustePrix.java
+++ LeJustePrix.java
@@ -1,8 +1,10 @@
 import java.util.Scanner;
+import java.util.Random;
 public class LeJustePrix{
     public static void main(String[] args) {
         Scanner clavier = new Scanner(System.in);
-        lireEntier(12,8);
+        //lireEntier(12,8);
+        générerEntierAléatBorné(4,10);
     }
     public static int lireEntier(int min, int max ) {
         Scanner clavier = new Scanner(System.in);
@@ -20,4 +22,14 @@ public class LeJustePrix{
         }
         return entier;
     }
+    public static int générerEntierAléatBorné(int min, int max){
+        if (min > max) {
+            throw new IllegalArgumentException("Le minimum est plus grand que le maximum :" + min + " " + max);
+        }
+        Random nb = new Random();
+        int nombreAleatoire = nb.nextInt(max - min + 1) + min;
+        System.out.println(nombreAleatoire);
+        return max;
+    }
+
 }
