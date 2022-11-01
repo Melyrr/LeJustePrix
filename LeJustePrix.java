42b8f60094b7e6eeacce268574fdacb37e3a98a1
Melyr <57968@etu.he2b.be>
2022-11-01 22:13:21 +0100

    Première partie

--- LeJustePrix.java
+++ LeJustePrix.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
 public class LeJustePrix{
     public static void main(String[] args) {
+        Scanner clavier = new Scanner(System.in);
+        lireEntier(12,8);
     }
-    
-
+    public static int lireEntier(int min, int max ) {
+        Scanner clavier = new Scanner(System.in);
+        if (min > max) {
+           int a = min;
+           min = max;
+           max = a;
+            System.out.println("Le minimum était grand que le maximum donc j'ai inversé les 2 valeurs. \nVoici maintenant le min : "+min+" "+"et le max : "+max);    
+        }
+        System.out.println("Veuillez entrez un nombre borné : ");
+        int entier = clavier.nextInt() ;
+        while (entier < min || entier > max) {
+            System.out.println("L'entier n'est pas compris entre " + min + " et " + max);
+            entier = clavier.nextInt() ;
+        }
+        return entier;
+    }
 }
